0842dc2ae967a5b009b053b1e09c2b1e27dd40ab
absolux <a.mouhassine@gmail.com>
2018-03-25 01:48:14 +0000

    Add `connect` benchmark

---
+++ apps/connect.js
@@ -0,0 +1,31 @@
+
+'use strict'
+
+var server
+var app = require('connect')()
+
+app.use((_, res) => {
+  res.end('Hello world!')
+})
+
+/**
+ * Launch the app
+ *
+ * @returns {Promise<Number>}
+ */
+exports.start = function start () {
+  return new Promise((resolve) => {
+    server = app.listen(0, resolve)
+  }).then(() => {
+    return server.address().port
+  })
+}
+
+/**
+ * Shut down the app
+ *
+ * @returns {Promise<void>}
+ */
+exports.stop = function stop () {
+  return new Promise((resolve) => server.close(resolve))
+}
